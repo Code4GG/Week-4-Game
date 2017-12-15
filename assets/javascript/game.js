9c0ea64626d98352c96048ee56dec8c99f02216f
code4GG <cavallaro.av@gmail.com>
2017-12-15 16:43:16 -0500

    convertedcode/function errors

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,119 +1,74 @@
 	
 	let wins = 1;
 	let losses = 1;
-	let correctNumber = randomNumber(19,120);
-	let purpleValue = randomNumber(1,12);
-	let clearValue = randomNumber(1,12);
-	let greenValue = randomNumber(1,12);
-	let yellowValue = randomNumber(1,12);
-	let totalScore;
+	let correctNumber;
+	let purpleValue;
+	let clearValue;
+	let greenValue;
+	let yellowValue;
+	
+	
 
-
-
-	//Random Number generator allows a plugin for number to match as well as crystals points
-	//through the parameters when it is called
 	function randomNumber(min, max){
 		
 		return Math.floor(Math.random() * max + min);
 	}
+	
 
-	function clear(){
-		$('#generatedNumber').empty();
-		$('#totalScore').empty();
+	function randomGeneratedNumber(){
+	    correctNumber = 0;
+		correctNumber = randomNumber(19, 120);
+		$('#generatedNumber').html(correctNumber);
 	}
 
-	function newGame(){
-		$('#generatedNumber').empty();
-		$('#winsNumber').empty();
-		$('#lossesNumber').empty();
-		$('#totalScore').empty(totalScore);
+	randomGeneratedNumber();
+
+	
+
+
+	function randomCrystalValue(){
+	 purpleValue = Math.floor((Math.random() * 12) + 1);
+	 // clearValue = randomNumber(1,12);
+	 // greenValue = randomNumber(1,12);
+	 // yellowValue = randomNumber(1,12);
+
+
+	 return $('#purple').val(purpleValue);
+	 console.log(purpleValue);
+	 // $('#clear').val(clearValue);
+	 // console.log(clearValue);
+	 // $('#green').val(greenValue);
+	 // console.log(greenValue);
+	 // $('#yellow').val(yellowValue);
+	 // console.log(yellowValue);
+
 
 	}
 
+	console.log(randomCrystalValue());
 
 
-	function startGame(){
-		// let correctNumber = randomNumber(19,120);
-		let totalScore = 0;
 
-	// 	function clear(){
-	// 	$('#generatedNumber').empty();
-	// 	$('#totalScore').empty();
-	// }
+	let totalScore = 0;
 
-	$('#generatedNumber').html(correctNumber);
+	// $('#totalScore').html(totalScore);
 
-	//crystals value generated between 1 - 12
-	//number trying to guess is between 19-120
+	$('.button').on('click', function(){
+		console.log(this);
+		totalScore = totalScore + parseInt($(this).val());
+		$('#totalScore').html(totalScore);
 
-
-	$('#purple').on('click', function(){
-		totalScore = totalScore + purpleValue;
-		console.log(totalScore);
-		$('#totalScore').html(" " + totalScore);
-
-		if (totalScore === correctNumber){
+		 if (totalScore === correctNumber){
 		$('#winsNumber').html(wins++)
-		$('#generatedNumber').html(correctNumber);
-		// correctNumber = ???
 
-	}
-
-	 else if (totalScore > correctNumber) {
+		}
+	 	else if (totalScore > correctNumber) {
 		$('#lossesNumber').html(losses++)}
-		$('#generatedNumber').html(correctNumber);
-		// correctNumber = ???
 		
-	})
-
-	
-	$('#clear').on('click', function(){
-		totalScore =  totalScore + clearValue;
-		console.log(totalScore);
-		$('#totalScore').html(" " + totalScore);
-
-		if (totalScore === correctNumber){
-		$('#winsNumber').html(wins++)
-	}
-	 else if (totalScore > correctNumber) {
-		$('#lossesNumber').html(losses++)}
-	
-	})
-
-	
-	$('#green').on('click', function(){
-		totalScore = totalScore + greenValue
-		console.log(totalScore);
-		$('#totalScore').html(" " + totalScore);
-
-		if (totalScore === correctNumber){
-		$('#winsNumber').html(wins++)
-	}
-	 else if (totalScore > correctNumber) {
-		$('#lossesNumber').html(losses++)}
 
 	})
 
-	
-	$('#yellow').on('click', function(){
-		totalScore = totalScore + yellowValue;
-		console.log(totalScore);
-		$('#totalScore').html(" " + totalScore);
-
-		if (totalScore === correctNumber){
-		$('#winsNumber').html(wins++)
-	}
-	 else if (totalScore > correctNumber) {
-		$('#lossesNumber').html(losses++)}
-	
-	})
-
-
-
-	}
-
-	startGame();
-		// newGame();
+			// newGame();
 
 
 
